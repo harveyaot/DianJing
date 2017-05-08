e73965cff333aba86aee0fb2b63f38f35bf99b4f
Wei He <hewe@microsoft.com>
2017-05-08 09:06:18 +0800

    crawl category individually

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -11,14 +11,15 @@ import redis
 # the main page refreh api 
 #Url = "http://www.toutiao.com/api/pc/feed/?category=__all__&utm_source=toutiao"
 #Url = "http://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1"
-Url = "http://www.toutiao.com/api/pc/feed/?category=__all__&utm_source=toutiao&widen=1&max_behot_time=%s&max_behot_time_tmp=%s&tadrequire=true&as=A11529B0AFB46A9&cp=590FD416EA79DE1"
+Url = "http://www.toutiao.com/api/pc/feed/?category=%s&utm_source=toutiao&widen=1&max_behot_time=%s&max_behot_time=%s&tadrequire=true&as=A135C940DF6BE90&cp=590FEB5EB9E01E1"
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 timestamp = "0"
-cats = ["new_tech","news_society","news_entertainment","news_sports","news_car","news_finance"]
+cats = ["news_tech","news_society","news_entertainment","news_sports","news_car","news_finance"]
 
 def process():
     global timestamp
-    toutiao_data = requests.get(Url%(timestamp,timestamp)).text
+    toutiao_data = requests.get(Url%(cats[cat],timestamp,timestamp)).text
+    #print Url%(cats[cat],timestamp,timestamp)
     data = json.loads(toutiao_data)
 
     if data.get("message") == "false":
@@ -44,4 +45,5 @@ def crawl():
         count += 1
 
 if __name__ == "__main__":
+    cat = int(sys.argv[1])
     crawl()
---
+++ scripts/run_batch_crawl.sh
@@ -0,0 +1,6 @@
+python  crawl.py 0 > logs/craw0.log &
+python  crawl.py 1 > logs/craw1.log &
+python  crawl.py 2 > logs/craw2.log &
+python  crawl.py 3 > logs/craw3.log &
+python  crawl.py 4 > logs/craw4.log &
+python  crawl.py 5 > logs/craw5.log &
