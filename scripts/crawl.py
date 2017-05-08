6d6e3c49e5cc4254319d24aa8d68fb8661f28e92
Wei He <hewe@microsoft.com>
2017-05-08 12:01:22 +0800

    add days for crawling

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -4,6 +4,7 @@ import requests
 import json
 import time
 import random
+import datetime
 
 import redis
 
@@ -11,21 +12,29 @@ import redis
 # the main page refreh api 
 #Url = "http://www.toutiao.com/api/pc/feed/?category=__all__&utm_source=toutiao"
 #Url = "http://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1"
+
 Url = "http://www.toutiao.com/api/pc/feed/?category=%s&utm_source=toutiao&widen=1&max_behot_time=%s&max_behot_time=%s&tadrequire=true&as=A135C940DF6BE90&cp=590FEB5EB9E01E1"
+
+cat = int(sys.argv[1])
+start_date = sys.argv[2]
+delta_days = int(sys.argv[3])
+
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-timestamp = "0"
+start_timestamp = datetime.datetime.strptime(start_date,"%Y%m%d").strftime("%s")
+end_timestamp = (datetime.datetime.strptime(start_date,"%Y%m%d") - datetime.timedelta(days=delta_days)).strftime("%s")
+timestamp = start_timestamp
 cats = ["news_tech","news_society","news_entertainment","news_sports","news_car","news_finance"]
 
 def process():
     global timestamp
     toutiao_data = requests.get(Url%(cats[cat],timestamp,timestamp)).text
-    #print Url%(cats[cat],timestamp,timestamp)
     data = json.loads(toutiao_data)
 
     if data.get("message") == "false":
         return 
 
     articals = data.get("data")
+    #timestamp = (datetime.datetime.fromtimestamp(int(timestamp)) - datetime.timedelta(minutes=15)).strftime("%s")
     timestamp = data.get("next").get("max_behot_time")
 
     for artical in articals:
@@ -37,7 +46,7 @@ def process():
 
 def crawl():
     count = 0 
-    while(count < 20000):
+    while(int(timestamp) > int(end_timestamp)):
         print "crawl num #[%d] with time stamp [%s]"%(count,timestamp)
         gap = random.randint(4,7)
         time.sleep(gap)
@@ -45,5 +54,4 @@ def crawl():
         count += 1
 
 if __name__ == "__main__":
-    cat = int(sys.argv[1])
     crawl()
--- scripts/run_batch_crawl.sh
+++ scripts/run_batch_crawl.sh
@@ -1,6 +1,6 @@
-python  crawl.py 0 > logs/craw0.log &
-python  crawl.py 1 > logs/craw1.log &
-python  crawl.py 2 > logs/craw2.log &
-python  crawl.py 3 > logs/craw3.log &
-python  crawl.py 4 > logs/craw4.log &
-python  crawl.py 5 > logs/craw5.log &
+python  crawl.py 0 20170509 1 > logs/craw0.log &
+python  crawl.py 1 20170509 1 > logs/craw1.log &
+python  crawl.py 2 20170509 1 > logs/craw2.log &
+python  crawl.py 3 20170509 1 > logs/craw3.log &
+python  crawl.py 4 20170509 1 > logs/craw4.log &
+python  crawl.py 5 20170509 1 > logs/craw5.log &
