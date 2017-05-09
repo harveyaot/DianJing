cd6423cd59bd5fd8188c93484076af45a0342933
Wei He <hewe@microsoft.com>
2017-05-09 11:26:25 +0800

    multi-process crawl toutiao's multiple cats news feed

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -11,12 +11,12 @@ import multiprocessing
 import redis
 
 
-# the main page refreh api 
-#Url = "http://www.toutiao.com/api/pc/feed/?category=__all__&utm_source=toutiao"
-#Url = "http://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1"
-
-Url = "http://www.toutiao.com/api/pc/feed/?category=%s&utm_source=toutiao&widen=1&max_behot_time=%s&max_behot_time=%s&tadrequire=true&as=A135C940DF6BE90&cp=590FEB5EB9E01E1"
+URL = "http://www.toutiao.com/api/pc/feed/?category=%s&utm_source=toutiao&widen=1&max_behot_time=%s&max_behot_time=%s&tadrequire=true&as=A135C940DF6BE90&cp=590FEB5EB9E01E1"
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+#sleep time in secs
+sleep_time = 5
+call_nums = 2000
 
 if len(sys.argv) > 1:
     start_date = sys.argv[1]
@@ -26,24 +26,21 @@ else:
 
 cats = ["news_tech","news_society","news_entertainment","news_sports","news_car",
         "news_finance","news_game","news_world","news_military","news_history",
-        "news_fashion","__all__"
-        ]
-cats2 = ["news_baby","news_food","news_health","news_story","news_travel"]
-cats3 = ["news_home"]
-cats = cats3
+        "news_fashion","news_baby","news_food","news_health","news_story",
+        "news_travel","new_home","__all__"]
 
 def process(cat,timestamp):
-    toutiao_data = requests.get(Url%(cat,timestamp,timestamp)).text
+    toutiao_data = requests.get(URL%(cat,timestamp,timestamp).text
     data = json.loads(toutiao_data)
 
     if data.get("message") == "false":
         return 
 
     articles = data.get("data")
-    #timestamp = (datetime.datetime.fromtimestamp(int(timestamp)) - datetime.timedelta(minutes=15)).strftime("%s")
     timestamp = data.get("next").get("max_behot_time")
 
     for article in articles:
+        # remove the invalid record
         if not article.get("group_id") or not article.get("title") or not article.get("abstract"):
             continue
         key = article.get("group_id")
@@ -55,9 +52,9 @@ def process(cat,timestamp):
 
 def worker(cat,timestamp):
     count = 0 
-    while(count < 20000):
+    while(count < call_nums):
         print "Thread:[%s] num#[%d] t:[%s]"%(cat,count,timestamp)
-        gap = random.randint(7,10)
+        gap = random.randint(sleep_time - 2,sleep_time + 2)
         time.sleep(gap)
         timestamp = process(cat,timestamp)
         count += 1
